30de1692873c9e1b0675ad4bf6ecce31ce605ecf
wamimi <nellycyberpro@gmail.com>
2025-01-20 23:57:13 +0300

    Added solution for struct and basic calculator

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,8 +4,20 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
+    let mut factorial:u32  = 1;
+    let mut i =1;
+
+    while 1 <= n{
+        factorial = factorial * i; 
+        i+= 1;
+        }
+        factorial
+    }
+
+
+
     todo!()
-}
+
 
 #[cfg(test)]
 mod tests {
--- exercises/03_ticket_v1/01_struct/src/lib.rs
+++ exercises/03_ticket_v1/01_struct/src/lib.rs
@@ -1,9 +1,13 @@
-// Define a struct named `Order` with the following fields:
-// - `price`, an unsigned integer
-// - `quantity`, an unsigned integer
-//
-// It should also have a method named `is_available` that returns a `true` if the quantity is
-// greater than 0, otherwise `false`.
+struct Order {
+    price: u32,
+    quantity: u32,
+}
+
+impl Order {
+    fn is_available(&self) -> bool {
+        self.quantity > 0
+    }
+}
 
 #[cfg(test)]
 mod tests {
